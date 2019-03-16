724fe75427cc12cd0c8a40bf1946427423a80700
Is_GK <dhsth@outlook.com>
2019-03-16 15:40:52 +0800

    跟新版本号

--- src/main/java/com/anotherera/magicrecipe/AnotherMagicRecipe.java
+++ src/main/java/com/anotherera/magicrecipe/AnotherMagicRecipe.java
@@ -19,7 +19,7 @@ public class AnotherMagicRecipe {
 
 	public static final String MODID = "anothermagicrecipe";
 	public static final String NAME = "Another Magic Recipe";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "0.0.2";
 
 	@Instance(AnotherMagicRecipe.MODID)
 	public static AnotherMagicRecipe instance;
